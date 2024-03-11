4876b239a99cc8dd7394cf17bf0754011a4413ab
Zouss <zakaria.bozari@hotmail.fr>
2024-03-11 23:53:19 +0000

    first push

---
+++ Djangotest/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def home_view(request):
+    context = {}  # You can pass context data to the template if needed
+    return render(request, 'HTML/index.html', context)
