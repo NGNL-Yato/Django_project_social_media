6caafc8009c302801a44ea7802da30f509375ad8
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2024-03-13 11:01:53 +0000

    -

--- Djangotest/views.py
+++ Djangotest/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 
 def home_view(request):
     context = {}  # You can pass context data to the template if needed
-<<<<<<< Updated upstream
     return render(request, 'HTML/home-extended.html', context)
-=======
-    return render(request, 'HTML/home.html', context)
 
 def index_view(request):
     context = {}  # You can pass context data to the template if needed
-    return render(request,'HTML/index.html',context)
->>>>>>> Stashed changes
+    return render(request,'HTML/home.html',context)
