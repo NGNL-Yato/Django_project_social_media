2401159f41eed24af4272ed79b1ae634ddb837dd
Zouss <zakaria.bozari@hotmail.fr>
2024-03-13 21:34:17 +0000

    took away extends

--- Djangotest/views.py
+++ Djangotest/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def home_view(request):
     context = {}  # You can pass context data to the template if needed
-    return render(request, 'HTML/home-extended.html', context)
+    return render(request, 'HTML/home.html', context)
 
 def index_view(request):
     context = {}  # You can pass context data to the template if needed
