704e09d22cc44be66d75da7e7d4caa76aeb0b4c2
Zouss <zakaria.bozari@hotmail.fr>
2024-03-13 13:12:19 +0000

    update num 1111111111

--- Djangotest/static/Javascript/homepage.js
+++ Djangotest/static/Javascript/homepage.js
@@ -1,6 +1,23 @@
 var darkButton = document.querySelector(".darkTheme");
+var darkmodeelements = {}
+var heading = document.getElementsByClassName("create-page ul li button heading",".messenger-search","fa-user-group","event .button","friend ul li button","explore a","see-more-btn")
+darkmodeactivate = false
 
 darkButton.onclick = function(){
     darkButton.classList.toggle("button-Active");
-    document.body.classList.toggle("dark-color")
+    document.body.classList.toggle("dark-color");
+    darkmodeelements = heading
+    for (var i = 0; i < darkmodeelements.length; i++) {
+        darkmodeelements[i].classList.toggle("dark-color");
+    }
+    console.log("Dark mode is active");
+    console.log(darkmodeelements)
+    if ( darkmodeactivate == false){
+        darkmodeactivate = true
+        console.log ("dark mode up")
+    } else {
+        darkmodeactivate = false
+        console.log ("dark mode down")
+    }   
 }
+
