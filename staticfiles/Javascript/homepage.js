d6c53ce459f8cdc6a417680bc2912fe98627853a
Zouss <zakaria.bozari@hotmail.fr>
2024-03-18 12:57:53 +0000

    _

--- staticfiles/Javascript/homepage.js
+++ staticfiles/Javascript/homepage.js
@@ -5,11 +5,10 @@ darkButton.onclick = function(){
     darkButton.classList.toggle("button-Active");
     document.body.classList.toggle("dark-color");
     console.log("Dark mode is active");
+    var textColors1 = document.querySelectorAll(".heading")
+    var textColors2 = document.querySelectorAll(".explore a")
     if (darkmodeactivate == false){
         darkmodeactivate = true
-        var textColors1 = document.querySelectorAll(".heading")
-        var textColors2 = document.querySelectorAll(".explore a")
-        console.log(textColors1)
         textColors1.forEach(function(textColor) {
             textColor.style.color = "white";
         });
@@ -20,9 +19,21 @@ darkButton.onclick = function(){
         console.log("dark mode up");
     } else {
         darkmodeactivate = false;
+        textColors1.forEach(function(textColor) {
+            textColor.style.color = "#6524c4";
+        });
+        console.log(textColors2)
+        textColors2.forEach(function(textColor) {
+            textColor.style.color = "#6524c4";
+        });
         console.log("dark mode down");
     }   
 }
 document.getElementById('iconBox3Button').addEventListener('click', function() {
-    document.getElementById('profileMenu').classList.toggle('show');
+    var profileMenu = document.getElementById('profileMenu');
+    var iconBox3Button = document.getElementById('iconBox3Button');
+    var rect = iconBox3Button.getBoundingClientRect();
+    profileMenu.style.top = (iconBox3Button.offsetTop + rect.height) + 'px'; // Adjusted this line
+    profileMenu.style.left = iconBox3Button.offsetLeft + 'px'; // Adjusted this line
+    profileMenu.classList.toggle('show');
 });
