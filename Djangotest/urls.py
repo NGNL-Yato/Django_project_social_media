97b417589193906e0119bb38047ef96253b7a364
Aya khalifi <aya.khalifi@etu.uae.ac.ma>
2024-03-30 05:55:58 +0000

    delete

--- Djangotest/urls.py
+++ Djangotest/urls.py
@@ -65,6 +65,10 @@ urlpatterns = [
     #
     path('create_Classroom_post/<str:uid>/', views.create_Classroom_post, name='createClassroomPost'),    
     #
+    path('deleteClassroom/<str:uid>',views.delete_Classroom,name='deleteClassroom'),
+    #
+    path('deleteClassroomPost/<int:id>',views.delete_ClassroomPost,name='deleteClassroomPost'),
+
     path('about/',views.index_view,name='about'),
     #
     path('chatApp/', views.chat_app_view, name='chat_app'),
--- Djangotest/views.py
+++ Djangotest/views.py
@@ -351,3 +351,17 @@ def chat(request):
     context = {}  
     return render(request,'HTML/Messaging/messages-page.html', context)
 
+def delete_Classroom(request, uid):
+    classroom = models.ClassRoom.objects.filter(UniqueinvitationCode=uid).first()
+    if classroom is not None:
+        classroom.delete()
+        return redirect('Classroom')
+
+#
+def delete_ClassroomPost(request, id):
+    post = models.PostClassroom.objects.filter(id=id).first()
+    if post is not None:
+            uid = post.classroom.UniqueinvitationCode
+
+            post.delete()
+            return redirect('Course',uid=uid)
